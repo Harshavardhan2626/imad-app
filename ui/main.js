69ae5173bb0badc33274b2a305b4a8ccedf0ab76
Harshavardhan2626 <harsh2626@gmail.com>
2017-09-02 10:22:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,9 @@ console.log('Loaded!');
 alert("The doom of valeria");
 var element=document.getElementById("main-text");
 element.innerHTML="Harsha is so cool";
+
+var img=documnet.getElementById("image");
+img.onclick=function ()
+{
+    img.style.marginLeft="100px";
+};
