4f9bd6a36516a1b2ceee8c481a0182adb0d0ae24
Harshavardhan2626 <harsh2626@gmail.com>
2017-09-02 10:30:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,13 @@ var element=document.getElementById("main-text");
 element.innerHTML="Harsha is so cool";
 
 var img=document.getElementById("image");
+marginleft=0;
+function moveright()
+{
+    marginleft=marginleft+10;
+    img.style.marginLeft=marginleft+'px';
+}
 img.onclick=function ()
 {
-    img.style.marginLeft="100px";
+    var interval=setInterval(moveright,50);
 };
