994a2f30985fdf87089ced8adcd9a6898d892505
Harshavardhan2626 <harsh2626@gmail.com>
2017-09-03 16:13:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,7 +22,14 @@ var nameinput=document.getElementById('name');
 var name=nameinput.value;
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
-    var names=["harsha","Musk","whiteshadow","name4"];
+     var request=new XMLHttpRequest();
+    request.onreadystatechange=function(){
+    if (request.readyState === XMLHttpRequest.DONE)
+    {
+        if (request.status ===200)
+        {
+            var names=request.responseText;
+            names=JSON.parse(names);
     var list='';
     for(i=0; i<names.length; i++)
         {
@@ -30,5 +37,13 @@ submit.onclick=function(){
     }
     var ul=document.getElementById('namelist');
     ul.innerHTML=list;
+            
+        }
+    }
+    };
+    request.open('GET','http://harsh2626.imad.hasura-app.io/submit-name?name='+name,true);
+    request.send(null);
+    
+  
 };
 
