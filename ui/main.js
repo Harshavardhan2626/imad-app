52db167efb920e25b4d07d07fa53fe57c3f4be90
Harshavardhan2626 <harsh2626@gmail.com>
2017-09-03 15:42:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,13 +22,13 @@ var nameinput=document.getElementById('name');
 var name=nameinput.value;
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
-    var names=["harsha","Musk","whiteshadow"];
+    var names=["harsha","Musk","whiteshadow","name4"];
     var list='';
-    for(i=0;i<names.length;i++)
+    for(i=0; i<names.length; i++)
         {
-    list+='<li>'+names[i]+'</li'>;    
+    list+='<li>'+names[i]+'</li>';    
     }
     var ul=document.getElementById('namelist');
     ul.innerHTML=list;
 };
-}
+
